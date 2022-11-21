18e53984de3462ee14ae05e48f2074adf23d466e
joel jose abraham <joeljoseabraham50@gmail.com>
2022-11-21 13:14:12 +0000

    Tweet csv

---
+++ twitter.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import csv
+from getpass import getpass
+
+
+driver = webdriver.Edge()
+
+driver.get('https://bing.com')
+
+element = driver.find_element(By.ID, 'sb_form_q')
+element.send_keys('WebDriver')
+element.submit()
+
+time.sleep(5)
+driver.quit()
